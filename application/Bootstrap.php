23c37bc3d45415a8849547d3247c794125be8ed7
kamilo <kamilo@kamilo-Presario-CQ43-Notebook-PC>
2012-04-20 11:22:22 -0500

    zona de retos: ranking y carta de reto parte 2

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,15 +3,6 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
-    protected function _initAutoload()
-    {
-        $moduleloader = new Zend_Application_Module_Autoloader(array(
-            'namespace' => '',
-            'basePath' => dirname(__FILE__),
-        ));
-        return $moduleloader;
-
-    }
     
     protected function _initViewHelpers()
     {
--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,7 @@ $(init);
 
 var carrera = [];
 var zonaretos = [];
+var notificaciones = [];
 
 function init(){
     initCarrera();
@@ -31,7 +32,12 @@ function initCarrera(){
 function initZonaRetos(){
     setUpZonaRetos();
     cargarJugadores();
+    //los retadores se cargan al hacer click en la carta
     cargarRetadores();
+    cargarNotificaciones();
+    $('#carta-reto-box #jugador').on('keyup click', function(){
+       buscarRetador($(this).val()); 
+    });
     $('#retar').fancybox();
 }
 
@@ -57,39 +63,12 @@ function setUpZonaRetos(){
     zonaretos.retadores = [];
 }
 
-function addRetador(nombre, idjugador){
+function addNotificacion(contenido){
     var tmp = [];
-    tmp.nombre = nombre;
-    tmp.idjugador = idjugador;
-    zonaretos.retadores.push(tmp);
+    tmp.contenido = contenido;
+    notificaciones.push(tmp);
 }
 
-function factoryRetadores(){
-    var retadores_html = [];
-    for(var i=0;i<zonaretos.retadores.length;i++){
-        if(i==0)
-            retadores_html.push('<tr>');
-        else
-            if(i>0 && i%4==0)
-                retadores_html.push('</tr><tr>');
-        retadores_html.push('<td class="wrap"><input type="radio" name="retado">'+zonaretos.retadores[i].nombre+'</td>');
-        if(i==(zonaretos.retadores.length - 1))
-             retadores_html.push('</tr>');
-    }
-    return retadores_html.join('');
-}
-
-function cargarRetadores(){
-    addRetador('Kamilo Cervantes',23);
-    addRetador('Kamilo Cervantes',23);
-    addRetador('Kamilo Cervantes',23);
-    addRetador('Kamilo Cervantes',23);
-    addRetador('Kamilo Cervantes',23);
-    addRetador('Kamilo Cervantes',23);
-    addRetador('Kamilo Cervantes',23);
-    addRetador('Kamilo Cervantes',23);
-    $('#jugadores table').append(factoryRetadores());
-}
 
 function addJugador(nombre,rank,puntos,online,idjugador){
     var tmp = [];
@@ -102,12 +81,58 @@ function addJugador(nombre,rank,puntos,online,idjugador){
     zonaretos.jugadores.push(tmp);
 }
 
+function addRetador(nombre, idjugador){
+    var tmp = [];
+    tmp.nombre = nombre;
+    tmp.idjugador = idjugador;
+    zonaretos.retadores.push(tmp);
+}
+
 function factoryJugadores(){
     var jugadores_html = '';
     for(var i=0;i<zonaretos.jugadores.length;i++){
         jugadores_html += '<tr><td>'+zonaretos.jugadores[i].nombre+'</td><td class="alinear">'+zonaretos.jugadores[i].rank+'</td><td class="alinear"><a href="#">Ver Cartas</a></td><td class="alinear">'+zonaretos.jugadores[i].puntos+'</td><td class="centrar"><img src="imgs/check.png"></td></tr>';
     }
     return jugadores_html;
+}
+
+function factoryNotificaciones(){
+    var notificaciones_html = [];
+    for(var i=0;i<notificaciones.length;i++){
+        notificaciones_html.push('<li><a href="#">'+notificaciones[i].contenido+'</a></li>');
+    }
+    return notificaciones_html.join('');
+}
+
+function factoryRetadores(retadores){
+    var retadores_html = [];
+    for(var i=0;i<retadores.length;i++){
+        if(i==0)
+            retadores_html.push('<tr>');
+        else
+            if(i>0 && i%4==0)
+                retadores_html.push('</tr><tr>');
+        retadores_html.push('<td><input type="radio" name="retado">'+retadores[i].nombre+'</td>');
+        if(i==(retadores.length - 1))
+             retadores_html.push('</tr>');
+    }
+    return retadores_html.join('');
+}
+
+function cargarNotificaciones(){
+    addNotificacion('Fabio14 te ha retado');
+    addNotificacion('Fabio14 te ha retado');
+    addNotificacion('Fabio14 te ha retado');
+    addNotificacion('Fabio14 te ha retado');
+    addNotificacion('Fabio14 te ha retado');
+    addNotificacion('Fabio14 te ha retado');
+    addNotificacion('Fabio14 te ha retado');
+    addNotificacion('Fabio14 te ha retado');
+    addNotificacion('Fabio14 te ha retado');
+    addNotificacion('Fabio14 te ha retado');
+    addNotificacion('Fabio14 te ha retado');
+    
+    $('#ul-main-arena').append(factoryNotificaciones());
 }
 
 function cargarJugadores(){
@@ -120,6 +145,34 @@ function cargarJugadores(){
 
     $('#ranking table').append(factoryJugadores());
 }
+
+function cargarRetadores(){
+    addRetador('Kamilo Cervantes',23);
+    addRetador('Juan Perez',23);
+    addRetador('adsadasdsadsw',23);
+    addRetador('23sddas33',23);
+    addRetador('sdfds55',23);
+    addRetador('dfdsfdsfvvvrtertres',23);
+    addRetador('Jose Lopex',23);
+    addRetador('Ana Milena Castaño',23);
+    $('#jugadores table').append(factoryRetadores(zonaretos.retadores));
+}
+
+function buscarRetador(parametro){
+    var tmp = [];
+    for(var i=0;i<zonaretos.retadores.length;i++){
+        if(zonaretos.retadores[i].nombre.toLowerCase().indexOf(parametro.toLowerCase()) >= 0){
+            tmp.push(zonaretos.retadores[i]);
+            console.log(zonaretos.retadores[i].nombre.toLowerCase()+' - '+zonaretos.retadores[i].nombre.toLowerCase().indexOf(parametro.toLowerCase()));
+        }
+    }
+    //console.log(tmp.join('-'));
+    $('#jugadores table tr').remove();
+    $('#jugadores table').append(factoryRetadores(tmp));
+    console.log(factoryRetadores(tmp));
+}
+
+
 
 /* fin zona de retos */
 
